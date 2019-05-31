a6ea8690ae55c0cdab68bf0ff0e2027a19c74073
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-31 17:36:40 +0000

    Automatically backed up by Learn

--- db/migrate/20190531173534_create_preferences.rb
+++ db/migrate/20190531173534_create_preferences.rb
@@ -1,9 +1,9 @@
 class CreatePreferences < ActiveRecord::Migration[5.0]
   def change
     create_table :preferences do |t|
-      t.string, :song_sort_order
-      t.string, :artist_sort_order
-      t.boolean, :allow_create_artists
+      t.string :song_sort_order
+      t.string :artist_sort_order
+      t.boolean :allow_create_artists
       t.boolean :allow_create_songs
 
       t.timestamps
